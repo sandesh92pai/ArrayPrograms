f789e6684677eb03985b5cdf452a115bac4ed172
= <=>
2023-02-25 10:25:29 +0530

    for each way

--- src/test/java/com/googly/tests/readingArrays.java
+++ src/test/java/com/googly/tests/readingArrays.java
@@ -9,9 +9,17 @@ public class readingArrays {
 	@Test
 	public void readArrayElement() {
 		int[] arr = {101,11,112,113,114};
-		
-		System.out.println(Arrays.toString(arr));
-		
+		System.out.println(Arrays.toString(arr));	
+	}
+	
+	// Using for each loop
+	
+	@Test
+	public void usingforEachLoop() {
+		int[] arr = {101,11,112,113,114};
+		for (int i : arr) {
+		System.out.println(i);
+		}
 	}
 
 }
